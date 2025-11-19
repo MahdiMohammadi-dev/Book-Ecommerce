8810259ece35ac9af8bfec2ec814b652eb69eaf8
Mahdi_Mohammadi <jnsm89@yahoo.com>
2025-11-20 00:51:50 +0330

    link project to Db

---
+++ KetabSara.DataLayer/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using KetabSara.DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KetabSara.DataLayer.Context;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- ketabSara/Program.cs
+++ ketabSara/Program.cs
@@ -1,7 +1,14 @@
+using KetabSara.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("KetabSaraDB")));
 
 var app = builder.Build();
 
