258eccbd9ceb49370ee596ea5d2427eb3bfe3d37
Mahdi_Mohammadi <jnsm89@yahoo.com>
2025-11-20 00:40:00 +0330

    init models

---
+++ KetabSara.DataLayer/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KetabSara.DataLayer.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+
+
+
+    public int BookId { get; set; }
+    [ForeignKey("BookId")]
+    public ICollection<Book> Books { get; set; }
+
+}
---
+++ KetabSara.DataLayer/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace KetabSara.DataLayer.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+
+
+
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+
+}
