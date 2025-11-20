3ea39dc8f90dc10afdb9edb61e0439735ec8cb05
Mahdi_Mohammadi <jnsm89@yahoo.com>
2025-11-20 12:56:27 +0330

    book repository with implementation class created

---
+++ KetabSara.DataLayer/Repositories/Book/BookRepository.cs
@@ -0,0 +1,55 @@
+﻿using KetabSara.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace KetabSara.DataLayer.Repositories.Book;
+
+    public class BookRepository:IBookRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Models.Book book)
+        {
+            var entity = book;
+           _context.Books.Add(entity);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task Edit(Models.Book book)
+        {
+            var entity = _context.Books.FirstOrDefault(x => x.Id == book.Id);
+            if (entity == null)
+                return;
+            entity.Title = book.Title;
+            entity.Description = book.Description;
+            entity.Price = book.Price;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = _context.Books.FirstOrDefault(x => x.Id == id);
+            if (entity == null)
+                return;
+            _context.Books.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Models.Book>> getAllBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public Models.Book findBookBy(int id)
+        {
+            var entity = _context.Books.FirstOrDefault(x => x.Id == id);
+            if (entity == null)
+                return null;
+            return entity;
+        }
+    }
+   
---
+++ KetabSara.DataLayer/Repositories/Book/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace KetabSara.DataLayer.Repositories.Book;
+
+public interface IBookRepository
+{
+    Task Create(Models.Book book);
+    Task Edit(Models.Book book);
+    Task Delete(int id);
+    Task<IEnumerable<Models.Book>> getAllBooks();
+    Models.Book findBookBy(int id);
+}
