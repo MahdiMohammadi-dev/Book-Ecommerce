c0fd9dcd7b91dbe1d28c736668e52054249400ea
Mahdi_Mohammadi <jnsm89@yahoo.com>
2025-11-20 17:36:49 +0330

    Add Termpleate to Project

---
+++ ketabSara/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ketabSara.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
